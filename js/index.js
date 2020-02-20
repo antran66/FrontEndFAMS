5966792f53a3a72c7cfbfb5d0214ae6475a9b9e8
dragonit96 <32900739+dragonit96@users.noreply.github.com>
2020-02-20 12:11:55 +0700

    upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready( function(){
+    // Collapse Button
+    $('.btn-expand-collapse').click(function(e) {
+        $('.navbar-primary').toggleClass('collapsed');
+        if($('.icon-none').css('display')=="none"){
+            $('.icon-none').css({display: 'block'});
+        }else{
+            $('.icon-none').css({display: 'none'});
+        }
+        if($('.role').css('display')=="none"){
+            $('.role').css({display: 'block'});
+        }else{
+            $('.role').css({display: 'none'});
+        }
+    });
+
+    //Send Request
+    $('a#candManager').click(function(){
+        $.get({
+            url: 'candidateList.html',
+            success: function(response){
+                $('.main-content').html(response);
+            }
+        });
+        
+        // prevent the link's default
+        return false;
+    });
+    $('a#dashBoard').click(function(){
+        window.location='index.html';
+    })
+});
